77d034cdeb4f34240f16afdf2d078ceb3203caf9
Thomas Strothjohann <thomas@strothjohann.net>
2015-03-17 16:48:00 +0000

    Mocked API data

--- test/gitUserSearchController.spec.js
+++ test/gitUserSearchController.spec.js
@@ -14,4 +14,28 @@ describe('GitUserSearchController', function() {
       expect(scope.searchResult).toBeUndefined();
       expect(scope.searchTerm).toBeUndefined();
     });
+
+
+    describe('when searching for a user', function() {
+      var items = [
+        {
+        "login": "tansaku",
+        "avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3",
+        "html_url": "https://github.com/tansaku"
+        },
+        {
+        "login": "stephenlloyd",
+        "avatar_url": "https://avatars.githubusercontent.com/u/196474?v=3",
+        "html_url": "https://github.com/stephenlloyd"
+        }
+      ];
+
+      it('should display search results', function(){
+        expect(scope.searchResult.items).toEqual(items);
+      });
+    });
+
+
+
+
 });
